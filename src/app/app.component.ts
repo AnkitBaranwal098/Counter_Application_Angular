917f98e95270c797037c0b5812fcba720d60a72f
AnkitBaranwal098 <ankitbaranwal1998@gmail.com>
2023-09-19 12:26:01 +0530

    second commit for counter application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  changeValue(event: any) {
+    console.log(event.target.name);
+    if (event.target.name == 'increement') {
+      this.value = this.value + 1;
+    } else if (event.target.name == 'decreement') {
+      if (this.value > 0) {
+        this.value = this.value - 1;
+      }
+    }
+  }
   title = 'counter_application';
+  value = 0;
+  plus = '+1';
+  minus = '-1';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
+import { CounterButtonComponent } from './counter-button/counter-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    CounterButtonComponent,
   ],
   imports: [
     BrowserModule
